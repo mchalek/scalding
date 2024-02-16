91a5d27578cfce9b69ab1c6bb527c75834f25fa6
Kevin McHale <kmchale@etsy.com>
2024-02-16 15:40:46 -0500

    satisfy ScaldingHfs compilation

--- scalding-core/src/main/java/com/twitter/scalding/tap/ScaldingHfs.java
+++ scalding-core/src/main/java/com/twitter/scalding/tap/ScaldingHfs.java
@@ -3,6 +3,7 @@ package com.twitter.scalding.tap;
 import java.io.IOException;
 
 import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.RecordReader;
 
@@ -10,6 +11,7 @@ import com.twitter.scalding.tuple.HadoopTupleEntrySchemeIterator;
 
 import cascading.flow.FlowProcess;
 import cascading.scheme.Scheme;
+import cascading.scheme.hadoop.SequenceFile;
 import cascading.tap.SinkMode;
 import cascading.tuple.Fields;
 import cascading.tuple.TupleEntryIterator;
@@ -18,40 +20,43 @@ public class ScaldingHfs extends cascading.tap.hadoop.Hfs {
   protected ScaldingHfs() {
   }
 
-  protected ScaldingHfs(Scheme<JobConf, RecordReader, OutputCollector, ?, ?> scheme) {
+  protected ScaldingHfs(Scheme<Configuration, RecordReader, OutputCollector, ?, ?> scheme) {
     super(scheme);
   }
 
   @Deprecated
   public ScaldingHfs(Fields fields, String stringPath) {
-    super(fields, stringPath);
+    super(new SequenceFile(fields), stringPath);
+    //super(fields, stringPath);
   }
 
   @Deprecated
   public ScaldingHfs(Fields fields, String stringPath, boolean replace) {
-    super(fields, stringPath, replace);
+    super( new SequenceFile( fields ), stringPath, replace ? SinkMode.REPLACE : SinkMode.KEEP );
+    //super(fields, stringPath, replace);
   }
 
   @Deprecated
   public ScaldingHfs(Fields fields, String stringPath, SinkMode sinkMode) {
-    super(fields, stringPath, sinkMode);
+    super(new SequenceFile(fields), stringPath, sinkMode);
+    //super(fields, stringPath, sinkMode);
   }
 
-  public ScaldingHfs(Scheme<JobConf, RecordReader, OutputCollector, ?, ?> scheme, String stringPath) {
+  public ScaldingHfs(Scheme<Configuration, RecordReader, OutputCollector, ?, ?> scheme, String stringPath) {
     super(scheme, stringPath);
   }
 
   @Deprecated
-  public ScaldingHfs(Scheme<JobConf, RecordReader, OutputCollector, ?, ?> scheme, String stringPath, boolean replace) {
-    super(scheme, stringPath, replace);
+  public ScaldingHfs(Scheme<Configuration, RecordReader, OutputCollector, ?, ?> scheme, String stringPath, boolean replace) {
+    super(scheme, stringPath, replace ? SinkMode.REPLACE : SinkMode.KEEP );
   }
 
-  public ScaldingHfs(Scheme<JobConf, RecordReader, OutputCollector, ?, ?> scheme, String stringPath, SinkMode sinkMode) {
+  public ScaldingHfs(Scheme<Configuration, RecordReader, OutputCollector, ?, ?> scheme, String stringPath, SinkMode sinkMode) {
     super(scheme, stringPath, sinkMode);
   }
 
   @Override
-  public TupleEntryIterator openForRead(FlowProcess<JobConf> flowProcess, RecordReader input) throws IOException {
+  public TupleEntryIterator openForRead(FlowProcess<? extends Configuration> flowProcess, RecordReader input) throws IOException {
     return new HadoopTupleEntrySchemeIterator(flowProcess, this, input);
   }
 }
